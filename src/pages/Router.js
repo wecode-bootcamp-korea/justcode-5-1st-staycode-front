58e449f7aac0fc23a44883e78486be4ec9919912
sanghyeon <jkn17083@gmail.com>
2022-06-20 15:22:09 +0900

    Add: 프론트 초기세팅완

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 import Home from './Home/Home';
 import Login from './Login/Login';
 import Signup from './Signup/Signup';
+import Basket from './Basket/Basket';
+import Detail from './Detail/Detail';
+import Find from './Find/Find';
+import Reservation from './Reservation/Reservation';
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/basket" element={<Basket />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/find" element={<Find />} />
+        <Route path="/reservation" element={<Reservation />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
